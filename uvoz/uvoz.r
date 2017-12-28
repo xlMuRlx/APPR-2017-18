455a0d847f560d963f51e1c159246d041b322fda
xlMuRlx <andraz.mur44@gmail.com>
2017-12-28 18:29:48 +0100

    Začel z vizualizacijo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -32,17 +32,26 @@ for (i in 1:ncol(prvenstva)) {
 }
 colnames(prvenstva) <- c("ekipa", "odigrane", "zmaga", "remi", "poraz", "st_dosezeni",
                          "st_prejeti", seq(1930, 1938, 4), seq(1950, 2014, 4))
-prvenstva <- prvenstva[-c(78, 79, 80), ]
+prvenstva <- prvenstva[-c(48, 78, 79, 80), ]
 prvenstva$zmaga <- round(prvenstva$zmaga)
 prvenstva <- prvenstva %>% add_row()
-prvenstva$ekipa[78] <- "Panama"
+prvenstva <- prvenstva %>% add_row()
+prvenstva$ekipa[77] <- "Panama"
+prvenstva$ekipa[78] <- "Iceland"
+prvenstva$odigrane[77] <- 0
 prvenstva$odigrane[78] <- 0
+prvenstva$zmaga[77] <- 0
 prvenstva$zmaga[78] <- 0
+prvenstva$remi[77] <- 0
 prvenstva$remi[78] <- 0
+prvenstva$poraz[77] <- 0
 prvenstva$poraz[78] <- 0
+prvenstva$st_dosezeni[77] <- 0
 prvenstva$st_dosezeni[78] <- 0
+prvenstva$st_prejeti[77] <- 0
 prvenstva$st_prejeti[78] <- 0
 prvenstva <- prvenstva %>% arrange(ekipa)
+
 
 ucinkovitost <- prvenstva[c(1, 6, 7)]
 ucinkovitost <- melt(ucinkovitost, id = "ekipa", na.rm = TRUE)
@@ -68,11 +77,13 @@ tekme <- tekme %>% arrange(ekipa)
 potegovanja <- uvrstitve[c(1, length(uvrstitve)-1)]
 colnames(potegovanja) <- c("ekipa", "st_nastopov")
 potegovanja$ekipa <- gsub("\\[([^)]*)\\]", "", potegovanja$ekipa)
+potegovanja$ekipa <- gsub("Bosnia and Herzegovina", "Bosnia & Herzegovina", potegovanja$ekipa)
 potegovanja$ekipa <- gsub("DR Congo", "Congo DR", potegovanja$ekipa)
 potegovanja$ekipa <- gsub("Ivory Coast", "Cote d Ivoire", potegovanja$ekipa)
 potegovanja$ekipa <- gsub("North Korea", "Korea DPR", potegovanja$ekipa)
 potegovanja$ekipa <- gsub("South Korea", "Korea Republic", potegovanja$ekipa)
 potegovanja <- potegovanja[-c(22), ]
 potegovanja <- potegovanja %>% arrange(ekipa)
+potegovanja$ekipa <- prvenstva$ekipa
 
 
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,29 @@
 # 3. faza: Vizualizacija podatkov
 
+prva <- tekme %>% filter(izid == "zmaga")
+prva <- prva[-2]
+prva <- prva %>% filter(stevilo > 10)
+graf.zmage <- ggplot(prva, aes(x = ekipa, y = stevilo)) + geom_bar(stat="identity")
+graf.zmage <- graf.zmage + xlab("Država") + ylab("Število zmag")
+
+druga <- data.frame(table(koncne.uvrstitve$ekipa))
+colnames(druga) <- c("ekipa", "st_udelezb")
+druga <- right_join(druga, potegovanja) %>% group_by(ekipa) %>% 
+  summarise(proc_uspesnost = round(st_udelezb / st_nastopov * 100, 2))
+graf.kvalificiranja <- ggplot(druga, aes(x = ekipa, y = proc_uspesnost)) + 
+  geom_bar(stat="identity")
+graf.kvalificiranja <- graf.zmage + xlab("Država") + ylab("Procentualna uspešnost")
+
+
+
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
-zemljevid <- pretvori.zemljevid(zemljevid)
+# zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
+#                              "OB/OB", encoding = "Windows-1250")
+# levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+#   { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+# zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
+# zemljevid <- pretvori.zemljevid(zemljevid)
 
 # Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+# povprecja <- druzine %>% group_by(obcina) %>%
+#  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
