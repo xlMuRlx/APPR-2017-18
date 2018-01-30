191eb141cb6e2fa89bb909491a3e53eeb254b918
xlMuRlx <andraz.mur44@gmail.com>
2018-01-30 17:25:58 +0100

    Zaključil napredno analizo.

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,22 +1,5 @@
 # 4. faza: Analiza podatkov
 
-zadetki <- read_excel("podatki/Zadetki.xlsx")
-colnames(zadetki) <- c("leto", "ekipe", "tekme", "stevilo", "povp_zadetki", "udelezba")
-zadetki$leto <- gsub("FIFA([^)]*)™", "", zadetki$leto)
-zadetki <- zadetki %>% arrange(leto)
-
-graf.zadetki <- ggplot(zadetki, aes(x = leto, y = stevilo)) + 
-  geom_point() + xlab("Leto") + ylab("Število zadetkov") + 
-  ggtitle("Število zadetkov na posameznem prvenstvu") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 0.5))
-
-graf.zadetki <- graf.zadetki + geom_smooth(method = lm, formula = stevilo ~ leto)
-
-napoved <- lm(data = zadetki[c(1, 4)], stevilo ~ leto)
-
-
-
-
 skupine <- left_join(peta %>% drop_na, tekme %>% filter(izid == "zmaga"))
 skupine <- skupine[-3] %>% arrange(ekipa)
 colnames(skupine) <- c("ekipa", "povp_uvrstitev", "st_zmage")
@@ -32,12 +15,53 @@ graf.povezava <- ggplot(razvrstitev, aes(x = ekipa,
 
 
 
-# podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-#                                 gostota.naselij = naselja/povrsina) %>%
-#   left_join(povprecja, by = "obcina")
-# row.names(podatki) <- podatki$obcina
-# podatki$obcina <- NULL
 
-# Število skupin
-# n <- 5
-# skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+zadetki <- read_excel("podatki/Zadetki.xlsx")
+colnames(zadetki) <- c("leto", "ekipe", "tekme", "st_golov", "povp_zadetki", "udelezba")
+zadetki$leto <- gsub("FIFA([^)]*)™", "", zadetki$leto)
+zadetki <- zadetki %>% arrange(leto)
+zadetki$leto <- as.numeric(zadetki$leto)
+zadetki$udelezba <- zadetki$udelezba * 1000
+
+
+graf.zadetki <- ggplot(zadetki, aes(x = leto, y = st_golov)) + 
+  geom_point() + xlab("Leto") + ylab("Število zadetkov") 
+graf.zadetki <- graf.zadetki + geom_smooth(method = 'lm', color = "red")
+
+
+pomozna.napoved <- lm(data = zadetki[c(1, 4)], st_golov ~ leto)
+leta <- data.frame(leto = seq(2018, 2026, 4))
+napoved <- leta %>% mutate(st_golov = round(predict(pomozna.napoved, .)))
+
+
+graf.povp_zadetki <- ggplot(zadetki, aes(x = leto, y = povp_zadetki)) + 
+  geom_point() + xlab("Leto") + ylab("Povprečno število zadetkov")
+graf.povp_zadetki <- graf.povp_zadetki + geom_smooth(method = 'lm', color = "red")
+
+pomozna.napoved2 <- lm(data = zadetki[c(1, 5)], povp_zadetki ~ leto)
+napoved <- napoved %>% mutate(povp_goli = round(predict(pomozna.napoved2, .), 2))
+
+
+graf.udelezba <- ggplot(zadetki, aes(x = leto, y = udelezba)) + 
+  geom_point() + xlab("Leto") + ylab("Udelezba")
+graf.udelezba <- graf.udelezba + geom_smooth(method = 'lm', color = "red")
+
+pomozna.napoved3 <- lm(data = zadetki[c(1, 6)], udelezba ~ leto)
+napoved <- napoved %>% mutate(udelezba = round(predict(pomozna.napoved3, .)))
+
+
+
+tabela.graf <- bind_rows(zadetki[c(1, 4)],napoved[c(1,2)])
+
+graf.porocilo <- ggplot(tabela.graf, aes(x = leto, y = st_golov)) + 
+  geom_point() + xlab("Leto") + ylab("Število zadetkov") +
+  ggtitle("Število zadetkov na posameznem prvenstvu") +
+  geom_smooth(method = 'lm', color = "red")
+
+
+
+
+colnames(napoved) <- c("Leto", "Število zadetkov", 
+                       "Povprečno število zadetkov na tekmo", "Udeležba")
+
+
