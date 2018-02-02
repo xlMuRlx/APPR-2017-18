ab22414d443c70c2b7fd3fc500bd4c5e69b23bde
xlMuRlx <andraz.mur44@gmail.com>
2018-02-02 15:26:21 +0100

    Začel shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,37 @@
+
+
+
+seznam <- as.list(skupine$povp_uvrstitev)
+names(seznam) <- skupine$ekipa
+
+
+
 library(shiny)
 
-shinyUI(fluidPage(
+fluidPage(
   
-  titlePanel("Slovenske občine"),
+  selectInput("select", label = h3("Izberite državo"), 
+              choices = seznam), 
   
-  tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
-    )
-))
+  hr(),
+  fluidRow(column(3, verbatimTextOutput("value")))
+  
+)
+
+
+
+#shinyUI(fluidPage(
+# 
+#  titlePanel("Slovenske občine"),
+#  
+#  tabsetPanel(
+#      tabPanel("Velikost družine",
+#               DT::dataTableOutput("druzine")),
+#      
+#      tabPanel("Število naselij",
+#               sidebarPanel(
+#                  uiOutput("pokrajine")
+#                ),
+#               mainPanel(plotOutput("naselja")))
+#    )
+#))
