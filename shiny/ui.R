147b628a56429d11171c3a81d3d2bfcd9c9b777c
xlMuRlx <andraz.mur44@gmail.com>
2018-02-03 18:02:13 +0100

    Manjši popravek

--- shiny/ui.R
+++ shiny/ui.R
@@ -2,13 +2,11 @@
 
 seznam <- as.list(pom_skupine$uvrstitev)
 for (i in 1:length(seznam)) {
-  seznam[i] = paste("Najboljša uvrstitev izbrane ekipe je ", 
+  seznam[i] = paste("Najboljša uvrstitev izbrane države je ", 
                     as.character(pom_skupine$uvrstitev[i]),
-                    ". mesto, ki ga je ekipa nazadnje dosegla leta ",
-                    as.character(pom_skupine$leto[i]), 
-                    " na prvenstvu ", 
-                    pom_skupine$prizorisce[i],
-                    ".", sep = "")
+                    ". mesto, ki ga je nazadnje dosegla leta ",
+                    as.character(pom_skupine$leto[i]), " na prvenstvu ", 
+                    pom_skupine$prizorisce[i], ".", sep = "")
 }
 names(seznam) <- pom_skupine$ekipa
 
