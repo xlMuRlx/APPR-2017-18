e35bfc9104b385192db7c94c63f67f7996d12842
xlMuRlx <andraz.mur44@gmail.com>
2018-02-05 16:58:21 +0100

    Zadnji popravki

--- shiny/server.R
+++ shiny/server.R
@@ -13,9 +13,10 @@ function(input, output) {
   
   output$grafi <- renderPlot({
     tabela <- koncne.uvrstitve %>% filter(ekipa == input$select)
-    print(ggplot(tabela,  aes(x = leto, y = uvrstitev), 
-                 xlim = c(1930, 2014), ylim = c(1, 32)) + geom_point(size = 5) + 
+    print(ggplot(tabela,  aes(x = leto, y = uvrstitev)) + geom_point(size = 5) + 
             xlab("Leto")+ ylab("Uvrstitev") + 
+            scale_x_continuous(breaks = seq(1930, 2014, 4), limits = c(1930, 2014)) +
+            scale_y_continuous(breaks = seq(1, 32, 2), limits = c(1, 32)) +
             ggtitle("Uvrstitve izbrane države na svetovnih prvenstvih"))
   })
 }
