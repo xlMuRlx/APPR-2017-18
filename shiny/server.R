994ba3221704995f0118e7321dcd08158f1bcb74
xlMuRlx <andraz.mur44@gmail.com>
2018-02-05 14:14:23 +0100

    Delo na shiny

--- shiny/server.R
+++ shiny/server.R
@@ -4,7 +4,18 @@ library(shiny)
 
 function(input, output) {
   
-  output$value <- renderPrint({input$select})
+  output$value <- renderText({
+    pom_skupine %>% filter(ekipa == input$select) %>%
+      transmute(text = paste0("Najboljša uvrstitev izbrane države je ", uvrstitev,
+                              ". mesto, ki ga je nazadnje dosegla leta ", leto,
+                              " na prvenstvu ", prizorisce, ".")) %>% as.character()
+  })
   
+  output$grafi <- renderPlot({
+    tabela <- koncne.uvrstitve %>% filter(ekipa == input$select)
+    print(ggplot(tabela, aes(x = round(leto), y = round(uvrstitev)), xlim = c(1930, 2014),
+                 ylim = c(1, 32)) + geom_point(size = 5) + 
+            xlab("Leto")+ ylab("Uvrstitev") + 
+            ggtitle("Uvrstitve izbrane države na svetovnih prvenstvih"))
+  })
 }
-
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,25 +1,15 @@
 
 
-seznam <- as.list(pom_skupine$uvrstitev)
-for (i in 1:length(seznam)) {
-  seznam[i] = paste("Najboljša uvrstitev izbrane države je ", 
-                    as.character(pom_skupine$uvrstitev[i]),
-                    ". mesto, ki ga je nazadnje dosegla leta ",
-                    as.character(pom_skupine$leto[i]), " na prvenstvu ", 
-                    pom_skupine$prizorisce[i], ".", sep = "")
-}
-names(seznam) <- pom_skupine$ekipa
-
-
-
 library(shiny)
 
 fluidPage(
   
   selectInput("select", label = h3("Izberite državo"), 
-              choices = seznam), 
+              choices = pom_skupine$ekipa), 
   
   hr(),
-  fluidRow(column(10, verbatimTextOutput("value")))
+  mainPanel(plotOutput("grafi")),
+  
+  fluidRow(column(10, textOutput("value")))
   
 )
